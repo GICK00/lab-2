4cc343f88614db8e326fd4df51afc67cc8918e52
GICK00 <utinsergei7212@gmail.com>
2024-12-18 22:01:50 +0300

    Готова lab-2

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, FlatList, RefreshControl, StyleSheet } from 'react-native';
+import LanguageItem from './components/LanguageItem';
 
 export default function App() {
+  const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState(languages);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setData([...languages]);
+      setRefreshing(false);
+    }, 1000);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <LanguageItem
+            name={item.name}
+            experience={item.experience}
+            logo={item.logo}
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
     </View>
   );
 }
@@ -13,8 +37,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+const languages = [
+  {
+    id: '1',
+    name: 'JavaScript',
+    experience: '3 года',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+  },
+  {
+    id: '2',
+    name: 'Python',
+    experience: '2 года',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+  },
+  {
+    id: '3',
+    name: 'Java',
+    experience: '1 год',
+    logo: 'https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg',
+  },
+];
